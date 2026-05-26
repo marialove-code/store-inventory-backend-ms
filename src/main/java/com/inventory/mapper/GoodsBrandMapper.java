972c75829f4f702a13858dd7ed3dd7bce2c622cd
周士林 <17018150@maria_love.user.noreply.gitee.com>
2026-05-26 21:28:56 +0800

    重新整理项目目录结构、包结构、模块分类

---
+++ src/main/java/com/inventory/mapper/GoodsBrandMapper.java
@@ -0,0 +1,18 @@
+package com.inventory.mapper;
+
+import com.inventory.entity.goods.GoodsBrand;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 95349
+* @description 针对表【goods_brand(商品品牌表)】的数据库操作Mapper
+* @createDate 2026-05-26 19:13:00
+* @Entity com.inventory.entity.goods.GoodsBrand
+*/
+public interface GoodsBrandMapper extends BaseMapper<GoodsBrand> {
+
+}
+
+
+
+
